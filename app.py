0740c8560a82cbbe4aeca0a3598c7f20b7eaaf3f
sakthivelm554@gmail.com <sakthivelm554@gmail.com>
2025-07-06 18:16:15 +0530

    Update /submittodoitem to handle Item ID, UUID, and Hash

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import uuid
 import hashlib
+import json
 
 app = Flask(__name__)
 
@@ -15,6 +16,19 @@ def get_data():
     with open('data.json', 'r') as f:
         data = json.load(f)
     return jsonify(data)
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    data = request.get_json()
+    item = {
+        'itemId': data['itemId'],
+        'itemUuid': data['itemUuid'],
+        'itemHash': data['itemHash'],
+        'itemName': data['itemName'],
+        'itemDescription': data['itemDescription']
+    }
+    todos.insert_one(item)
+    return jsonify({'message': 'Todo item added successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
